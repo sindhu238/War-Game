5e8ea1c727948eb11a83d7626eee60edadbbbb81
Venkateswara <venkateswara@venkatewarasmbp.home>
2016-11-18 10:59:56 +0000

    saved all changes 1

--- War Game/ViewController.swift
+++ War Game/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leftCardImageView: UIImageView!
+    @IBOutlet weak var rightCardImageView: UIImageView!
+    @IBOutlet weak var playerScoreLabel: UILabel!
+    @IBOutlet weak var cpuScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
+    
+    let cardValues : [String] = ["card2", "card3", "card4", "card5", "card6"
+        , "card7", "card8", "card9", "card10", "jack", "queen", "king", "ace"]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +32,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dealButton(_ sender: UIButton) {
+        
+        let leftCardNumber: Int = Int(arc4random_uniform(13))
+        let rightCardNumber: Int = Int(arc4random_uniform(13))
+        
+        leftCardImageView.image = UIImage(named: cardValues[leftCardNumber])
+        rightCardImageView.image = UIImage(named: cardValues[rightCardNumber])
+        
+        if leftCardNumber > rightCardNumber {
+            leftScore += 1
+            playerScoreLabel.text = String(leftScore)
+        }
+        else if leftCardNumber < rightCardNumber {
+            rightScore += 1
+            cpuScoreLabel.text = String(rightScore)
+        }
+        
+    }
 
 }
 
